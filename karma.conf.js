4ea7593d953d384d71e1c2715ab17f1e7890fb17
rogues <rogues@es6.today>
2017-02-15 22:29:45 -0600

    v0.1.0-beta.1 – Setup (#26)
    0.1.0/SettingUpTheEnvironment
    2a922fe: [setup] Enable JSHint and Continuous Integration.
     - Ignore unnecessary files and folders.
     - Create package.json for npm.
     - Install jshint library.
     - Add .jshintrc configuration file.
     - Add .travis.yml configuration file.
    0de94bc: [build] Automatically run tests before every commit with ghooks.
    8fafe6a: [tool] Install EditorConfig on the project.
    4d5a02a: [tool] Add ES6 support.
    5eb2b2f: [tool] Enable JavaScript Unit Testing.
    b82995f: [tool] Configure Browserify in project.
    f50146a: [tool] Enable ES6 in Karma and project.
    c1581fc: [build] Ignore coverage folder.
    d0b4fde: [tool] Enable Code Coverage with Istanbul and ES6.
    1c127df: [feat] Add an alias configuration to use absolute paths in files.
    50a983f: [tool] Add configuration to generate docs by JSDoc.

---
+++ karma.conf.js
@@ -0,0 +1,55 @@
+var istanbul = require('browserify-istanbul');
+
+const alias = {
+  aliases: {
+    'src': './src',
+  },
+  verbose: false,
+};
+
+module.exports = function (config) {
+  config.set({
+    frameworks: ['browserify', 'jasmine'],
+    files: [
+      'src/**/*.js',
+      'tests/**/*.spec.js'
+    ],
+    preprocessors: {
+      'tests/**/*.js': ['jshint', 'browserify'],
+      'src/**/*.js': ['jshint', 'browserify']
+    },
+    browsers: ['PhantomJS'],
+    browserify: {
+      debug: true,
+      transform: [['babelify', {
+        ignore: /node_modules/
+      }], istanbul({
+        ignore: ['tests/**', '**/node_modules/**'],
+        instrumenterConfig: {
+          embedSource: true
+        }
+      }), ['aliasify', alias]],
+      extensions: ['.js']
+    },
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      instrumenters: {isparta: require('isparta')},
+      instrumenter: {
+        'src/**/*.js': 'isparta'
+      },
+      reporters: [
+        { type: 'text' },
+        { type: 'html', dir: 'coverage', subdir: 'html' },
+        { type: 'lcovonly', dir: 'coverage', subdir: 'lcov' }
+      ],
+      check: {
+        global: {
+          statements: 100,
+          branches: 100,
+          functions: 100,
+          lines: 100
+        }
+      },
+    }
+  });
+};
---
+++ tests/sayHello.spec.js
@@ -0,0 +1,7 @@
+import sayHello from 'src/index';
+
+describe('sayHello function', () => {
+  it('returns a salute', () => {
+    expect(sayHello('ro9ues')).toBe('Hello ro9ues!');
+  });
+});
