a0d855f27482b936d68a9d0ee7641ead5445b451
Rogues <rogues@es6.today>
2017-02-19 10:38:59 -0600

    [scope] Watching Object Properties: $watch and $digest.

--- tests/scope.spec.js
+++ tests/scope.spec.js
@@ -7,4 +7,22 @@ describe('Scope', () => {
 
     expect(scope.aProperty).toBe(1);
   });
+
+  describe('$digest', () => {
+    let scope;
+
+    beforeEach(() => {
+      scope = new Scope();
+    });
+
+    it('calls the listener function of a watch on first $digest', () => {
+      const watchExpression = () => 'watchExpression';
+      const listener        = jasmine.createSpy();
+
+      scope.$watch(watchExpression, listener);
+      scope.$digest();
+
+      expect(listener).toHaveBeenCalled();
+    });
+  });
 });
