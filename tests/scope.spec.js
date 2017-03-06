e59de3a9c2d04d416b6212a8d2c3cd130deadbf1
Rogues <rogues@es6.today>
2017-03-06 00:51:45 -0600

    [scope] Does not end  so that new watches are not run.

--- tests/scope.spec.js
+++ tests/scope.spec.js
@@ -174,5 +174,25 @@ describe('Scope', () => {
       scope.$digest();
       expect(watchExecutions).toBe(301);
     });
+
+    it('does not end $digest so that new watches are not run', () => {
+      scope.aValue  = 'abc';
+      scope.counter = 0;
+
+      scope.$watch(
+        scope => scope.aValue,
+        (newValue, oldValue, scope) => {
+          scope.$watch(
+            scope => scope.aValue,
+            (newValue, oldValue, scope) => {
+              scope.counter++;
+            }
+          );
+        }
+      );
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+    });
   });
 });
